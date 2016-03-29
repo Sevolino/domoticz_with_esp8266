c8fef2c82c426b95e877c0a395b006188866c080
jipech <jipech@centrum.cz>
2016-03-29 10:28:48 +0200

    Update domoticz.lua

--- thermometer/domoticz.lua
+++ thermometer/domoticz.lua
@@ -6,7 +6,7 @@
  t = r * 285 / 1024
  conn=net.createConnection(net.TCP, 0)
  conn:on("receive", function(conn, payload) print(payload) end )
- conn:connect(8080,"192.168.1.230")
- conn:send("GET /json.htm?type=command&param=udevice&idx=1&nvalue=0&svalue=" .. t .. " HTTP/1.1\r\nHost: 10.0.42.13\r\n"
+ conn:connect(8080,"IP")
+ conn:send("GET /json.htm?type=command&param=udevice&idx=IDX&nvalue=0&svalue=" .. t .. " HTTP/1.1\r\nHost: 10.0.42.13\r\n"
  .."Connection: keep-alive\r\nAccept: */*\r\n\r\n")
  end)
